ec8d5d24660ecaeb56ffcea17dbe7cc7608bf401
Ivan <ivanrj7j@gmail.com>
2021-10-30 15:46:42 +0530

    made the vector arithmetically operable

--- Vector.py
+++ Vector.py
@@ -4,4 +4,11 @@ class Vector():
         self.y = y
     def vector(self):
         return (self.x, self.y)
-
+    def __add__(self, other:tuple):
+        return(self.x + other[0], self.y+other[1])
+    def __mul__(self, other:tuple):
+        return(self.x * other[0], self.y * other[1])
+    def __sub__(self, other:tuple):
+        return(self.x - other[0], self.y - other[1])
+    def __truediv__(self, other:tuple):
+        return(self.x / other[0], self.y / other[1])
