c8604425592fb6110841e9eb0d0f6294a9eb58fe
Ivan <ivanrj7j@gmail.com>
2021-11-01 10:59:23 +0530

    made func to find angle and distance of 2 vectors

--- Physics.py
+++ Physics.py
@@ -61,3 +61,4 @@ class Physics():
 
     def calculateAccelaration(self, dt):
        pass
+
--- Vector.py
+++ Vector.py
@@ -1,9 +1,14 @@
+import math
+
 class Vector():
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
+
     def vector(self):
         return (self.x, self.y)
+        # returns a tuple of the vector 
+
     def __add__ (self, other):
         if type(other) == tuple:
             return Vector(self.x + other[0], self.y+other[1])
@@ -13,6 +18,8 @@ class Vector():
             return Vector(self.x + other.x, self.y + other.y)
         else:
             raise(f"Operation not supported with vector and {type(other)}")
+        # handles addition 
+
     def __mul__(self, other):
         if type(other) == tuple:
             return Vector(self.x * other[0], self.y * other[1])
@@ -22,6 +29,8 @@ class Vector():
             return Vector(self.x + other.x, self.y + other.y)
         else:
             raise(f"Operation not supported with vector and {type(other)}")
+        # handles multiplication 
+
     def __sub__(self, other):
         if type(other) == tuple:
             return Vector(self.x - other[0], self.y - other[1])
@@ -31,6 +40,8 @@ class Vector():
             return Vector(self.x + other.x, self.y + other.y)
         else:
             raise(f"Operation not supported with vector and {type(other)}")
+        # handles substraction 
+
     def __truediv__(self, other):
         if type(other) == tuple:
             return Vector(self.x / other[0], self.y / other[1])
@@ -40,8 +51,25 @@ class Vector():
             return Vector(self.x + other.x, self.y + other.y)
         else:
             raise(f"Operation not supported with vector and {type(other)}")
+        # handles division 
+
     def __str__(self) -> str:
         return str((self.x, self.y))
+        # returns a string of the object when needed 
+
+
+
+def calculateAngleBetweenTwoVectors(vectorA:Vector, vectorB:Vector):
+    dotProduct = vectorA.x*vectorB.x + vectorA.y*vectorB.y
+    modOfVector1 = math.sqrt( vectorA.x*vectorA.x + vectorA.y*vectorA.y)*math.sqrt(vectorB.x*vectorB.x + vectorB.y*vectorB.y) 
+    angle = dotProduct/modOfVector1
+    angleInDegree = math.degrees(math.acos(angle))
+    return angleInDegree
+
+def calculateDistanceBetweenTwoVectors(a:Vector, b:Vector):
+    distance = math.sqrt(((b.x - a.x)**2) + ((b.y - a.y)**2))
+    return distance
 
 if __name__ == "__main__":
-    pass
+    print(calculateAngleBetweenTwoVectors(Vector(10, 10), Vector(10, 20)))
+    print(calculateDistanceBetweenTwoVectors(Vector(10, 10), Vector(-69, 500)))
