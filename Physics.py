501f3c1711241e60a295210df635ef6f0153c84f
Ivan <ivanrj7j@gmail.com>
2021-11-02 10:05:06 +0530

    commented on everything on the physics module

--- Physics.py
+++ Physics.py
@@ -18,56 +18,88 @@ class Physics():
         self.doesapplyGravity = True
         self.kineticEnergy = 1
         self.centreOfMass = centreOfMass
+        # Initialising all the variables 
 
     def applyGravity(self, dt):
+        """
+        Applies gravitational force to the object 
+        """
         if self.doesapplyGravity:
             self.velocity += (0, self.defaultGravityAccelaration*self.gravityScale*self.airDrag)
+            # accelarating object 
             self.object.x += self.velocity.x * dt
             self.object.y += self.velocity.y * dt
-            rotation = 0
+            # moving the object 
+            # calculating the motion 
+
             radius = calculateDistanceBetweenTwoVectors(self.centreOfMass, Vector(0, self.object.width / 2))
+            # calculating distance between centre of mass and top of the object 
             force_applying = ((self.defaultGravityAccelaration*self.gravityScale*self.airDrag) * self.mass)
+            # calculating the force applied to the object 
             angle = calculateAngleBetweenTwoVectors(self.centreOfMass, Vector(0, self.object.width / 2))
-            rotation = radius * force_applying * math.sin(angle)
-            print(rotation *(dt / 60))
+            # calculating the angle between centre of mass and top of the object 
+            rotation = radius * force_applying * math.sin(angle) * (dt / 60)
+            # finding the rotation 
             self.surface = pygame.transform.rotate(self.surface, rotation)
+            # rotating the object 
+
+            # calculating the rotation 
 
 
         
     def applyForce(self, dt ,force:Vector, considerMass = True, kineticEnergy = 1, applyKineticEnergy = False):
+        """Applies a force to the object"""
         if applyKineticEnergy:
             self.kineticEnergy = kineticEnergy
             # applies kinetic energy 
+
         if considerMass:
             self.velocity += force/self.mass
             self.move(dt)
         else :
             self.velocity += force
             self.move(dt)
+        # calculating the velocity 
         
 
     def move(self, dt):
+        """
+        Moves the object with the current velocity
+        """
         self.object.x += self.velocity.x * dt
         self.object.y += self.velocity.y * dt
         
 
     def collision(self, dt):
+        """
+        Handles collision
+        """
         for collisionBody in self.collisionObjects:
             if self.object.bottom >= collisionBody.y:
+                """
+                does this every time when the bottom of the object is colliding with the
+                top of any other object
+                """
                 self.velocity = Vector(0,0)
                 self.doesapplyGravity = False
+                # stopping the gravity force 
                 if self.kineticEnergy <= 1 and self.kineticEnergy >=0:
                     self.kineticEnergy -= ((self.defaultGravityAccelaration*self.gravityScale*self.airDrag) * self.mass * self.object.height) / 1000
                     if self.kineticEnergy < 0:
                         self.kineticEnergy = 0
+                # reducing the kinetic energy of the object 
                 force = Vector(0,-(self.defaultGravityAccelaration*self.gravityScale*self.airDrag)) * self.mass * self.object.height / 5 
                 force = Vector(force.x * self.kineticEnergy, force.y * self.kineticEnergy)
                 self.applyForce(force=force, considerMass=False, dt=dt)
+                # applying the reaction force to the object 
             else:
                 self.doesapplyGravity = True
+                # makes the object affected by gravity when it is not collideed 
 
             if self.object.bottom >= collisionBody.y:
                 self.object.y = collisionBody.y - self.object.height
+                # stopping the overlaping of 2 object 
+                
     def calculateVelocityandForce(self,dt):
         pass
 
