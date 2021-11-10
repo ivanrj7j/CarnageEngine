01175e2f451f24c9aa14d1714501b13e47c2a4d2
Ivan <ivanrj7j@gmail.com>
2021-11-10 11:00:27 +0530

    now can convert vector3 to vector2 and vice versa

--- CarnageEngine/Vector.py
+++ CarnageEngine/Vector.py
@@ -35,9 +35,9 @@ class Vector2():
         if type(other) == tuple:
             return Vector2(self.x - other[0], self.y - other[1])
         elif type(other) == int or type(object) == float:
-            return Vector2(self.x + other, self.y - other)
+            return Vector2(self.x - other, self.y - other)
         elif type(other) == Vector2:
-            return Vector2(self.x + other.x, self.y + other.y)
+            return Vector2(self.x - other.x, self.y - other.y)
         else:
             raise(f"Operation not supported with Vector2 and {type(other)}")
         # handles substraction 
@@ -48,7 +48,7 @@ class Vector2():
         elif type(other) == int or type(object) == float:
             return Vector2(self.x / other, self.y / other)
         elif type(other) == Vector2:
-            return Vector2(self.x + other.x, self.y + other.y)
+            return Vector2(self.x / other.x, self.y / other.y)
         else:
             raise(f"Operation not supported with Vector2 and {type(other)}")
         # handles division 
@@ -59,6 +59,73 @@ class Vector2():
 
     def zero():
         return Vector2(0,0)
+
+    def convertToVector3(self):
+        return Vector3(self.x, self.y, 0)
+
+class Vector3():
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def vector(self):
+        return (self.x, self.y)
+        # returns a tuple of the vector 
+
+    def __add__ (self, other):
+        if type(other) == tuple:
+            return Vector3(self.x + other[0], self.y+other[1], self.z + other[2])
+        elif type(other) == int or type(object) == float:
+            return Vector3(self.x + other, self.y + other, self.z + other)
+        elif type(other) == Vector3:
+            return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+        else:
+            raise(f"Operation not supported with vector and {type(other)}")
+        # handles addition 
+
+    def __mul__(self, other):
+        if type(other) == tuple:
+            return Vector3(self.x * other[0], self.y*other[1], self.z * other[2])
+        elif type(other) == int or type(object) == float:
+            return Vector3(self.x * other, self.y * other, self.z * other)
+        elif type(other) == Vector3:
+            return Vector3(self.x * other.x, self.y * other.y, self.z * other.z)
+        else:
+            raise(f"Operation not supported with vector and {type(other)}")
+        # handles multiplication 
+
+    def __sub__(self, other):
+        if type(other) == tuple:
+            return Vector3(self.x - other[0], self.y - other[1], self.z - other[2])
+        elif type(other) == int or type(object) == float:
+            return Vector3(self.x - other, self.y - other, self.z - other)
+        elif type(other) == Vector3:
+            return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+        else:
+            raise(f"Operation not supported with Vector2 and {type(other)}")
+        # handles substraction 
+
+    def __truediv__(self, other):
+        if type(other) == tuple:
+            return Vector3(self.x / other[0], self.y / other[1], self.z / other[2])
+        elif type(other) == int or type(object) == float:
+            return Vector3(self.x / other, self.y / other, self.z / other)
+        elif type(other) == Vector3:
+            return Vector3(self.x / other.x, self.y / other.y, self.z / other.z)
+        else:
+            raise(f"Operation not supported with Vector2 and {type(other)}")
+        # handles division 
+
+    def __str__(self) -> str:
+        return str((self.x, self.y, self.z))
+        # returns a string of the object when needed 
+
+    def zero():
+        return Vector3(0,0,0)
+
+    def convertToVector2(self):
+        return Vector2(self.x, self.y)
 
 def calculateAngleBetweenTwoVectors(vectorA:Vector2, vectorB:Vector2):
     dotProduct = vectorA.x*vectorB.x + vectorA.y*vectorB.y
